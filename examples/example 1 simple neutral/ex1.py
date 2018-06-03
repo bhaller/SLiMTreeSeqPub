5c583965c0dfedfc8d5d64197d4b26d1d8054ab8
Ben Haller <github@sticksoftware.com>
2018-06-03 14:38:40 +0900

    make a python script to run example 1

---
+++ examples/example 1 simple neutral/ex1.py
@@ -0,0 +1,63 @@
+import os, subprocess, msprime
+from timeit import default_timer as timer   # import issues with timeit.timeit() are too annoying...
+
+# the PyCharm console doesn't seem to set up the working directory where we want it; I use this to fix that problem
+# examples_dir = "/Users/bhaller/DocumentsSynced/Research/Messer lab/Publication TreeSeq/SLiMTreeSeqPub/examples/"
+# os.chdir(examples_dir + "example 1 simple neutral")
+
+
+def overlay_mutations(infile, outfile, rate, seed=1):
+    ts = msprime.load(infile)
+    rng = msprime.RandomGenerator(seed)
+    nodes = msprime.NodeTable()
+    edges = msprime.EdgeTable()
+    migrations = msprime.MigrationTable()
+    sites = msprime.SiteTable()
+    mutations = msprime.MutationTable()
+    provenances = msprime.ProvenanceTable()
+    individuals = msprime.IndividualTable()
+    populations = msprime.PopulationTable()
+    ts.dump_tables(nodes=nodes, edges=edges, migrations=migrations, sites=sites, mutations=mutations,
+                   provenances=provenances)
+    mutgen = msprime.MutationGenerator(rng, rate)
+    mutgen.generate(nodes, edges, sites, mutations)
+    mutated_ts = msprime.load_tables(nodes=nodes, edges=edges, sites=sites, mutations=mutations,
+                                     provenances=provenances, individuals=individuals, populations=populations)
+    mutated_ts.dump(outfile)
+
+
+# Run SLiM without tree-sequence recording: ex1_noTS.slim
+# Model results will be saved to ./ex1_noTS.slimout
+start = timer()
+subprocess.check_output(["../slim", "-m", "-s", "0", "./ex1_noTS.slim"])
+time_noTS = timer() - start
+print("Time for SLiM without tree-sequence recording: " + str(time_noTS) + "\n")
+
+
+# Run SLiM with tree-sequence recording: ex1_TS.slim
+# Model results will be saved to ./ex1_TS.trees
+# Then overlay neutral mutations onto ./ex1_TS.trees with msprime and output to ./ex1_TS_overlaid.trees
+start = timer()
+subprocess.check_output(["../slim", "-m", "-s", "0", "./ex1_TS.slim"])
+time_TS = timer() - start
+print("Time for SLiM with tree-sequence recording: " + str(time_TS))
+
+start = timer()
+overlay_mutations("./ex1_TS.trees", "./ex1_TS_overlaid.trees", 1e-7)
+time_overlay = timer() - start
+print("Time for msprime mutation overlay: " + str(time_overlay))
+
+print("Total time for SLiM + msprime: " + str(time_TS + time_overlay) + "\n")
+
+
+# Use msprime to do a plain coalescent simulation with equivalent parameters
+# Model results will be saved to ./ex1_msprime.trees
+start = timer()
+ts = msprime.simulate(sample_size=500, Ne=500, length=1e8, recombination_rate=1e-8, mutation_rate=1e-7)
+ts.dump("./ex1_msprime.trees")
+time_coalescent = timer() - start
+print("Total time for coalescent: " + str(time_coalescent))
+
+
+
+
