89e49d5df53b17338cc60d04aa067399ecd9842d
Ben Haller <github@sticksoftware.com>
2018-07-01 06:49:00 +0900

    fix sample size in msprime calls

--- examples/example 4 neutral burn-in/msp_coalescent_ex4.py
+++ examples/example 4 neutral burn-in/msp_coalescent_ex4.py
@@ -5,5 +5,7 @@ This is the alternative method that we present a time for in the paper
 '''
 
 import msprime
-ts = msprime.simulate(sample_size=100000, Ne=100000, length=1e8, recombination_rate=1e-8, mutation_rate=1e-7)
+
+# note that sample_size is haploid, so it needs to be double Ne to produce n=Ne is diploid terms
+ts = msprime.simulate(sample_size=200000, Ne=100000, length=1e8, recombination_rate=1e-8, mutation_rate=1e-7)
 ts.dump("ex4_msprime.trees")
